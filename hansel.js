e7dde5c93600b12a6c3913ae2c7472b5fb27c867
Nicholas Blott <blottn@tcd.ie>
2024-02-10 11:49:49 +0100

    Use dual ws

--- hansel.js
+++ hansel.js
@@ -2,6 +2,7 @@
 
 import { WebSocket } from 'ws'
 import { open, readFile } from 'fs/promises';
+import { join } from 'path';
 import { apply } from 'json-merge-patch';
 
 import { ArgumentParser } from 'argparse';
@@ -9,7 +10,7 @@ import { ArgumentParser } from 'argparse';
 let parser = new ArgumentParser();
 
 parser.add_argument('--data-dir', {default: './data'});
-parser.add_argument('--ws-url', {default: 'wss://wrm.blottn.ie/hansel/test'});
+parser.add_argument('--ws-url', {default: 'ws://localhost:3141/hansel/test'});
 
 let { data_dir, ws_url } = parser.parse_args();
 
@@ -18,21 +19,38 @@ let db = JSON.parse(await readFile(`${data_dir}/db.json`));
 let log = await open(`${data_dir}/diffs.jsonl`, 'a');
 
 // Initialise websocket
-const url = `${ws_url}`;
-const ws = new WebSocket(url);
-ws.on('error', console.error);
-ws.on('open', () => {
-  console.log(`Connected to wss: ${url}`)
+const in_url = ws_url;
+const out_url = join(ws_url, 'trail');
+const ws_in = new WebSocket(in_url);
+const ws_out = new WebSocket(out_url);
+ws_in.on('error', console.error);
+
+(await Promise.all([new Promise((res) => {
+  ws_out.on('open', () => {
+    res(`Connected to ws_out: ${out_url}`);
+  });
+}), new Promise((res) => {
+  ws_in.on('open', () => {
+    res(`Connected to ws_in: ${in_url}`);
+  });
+})])).forEach(m => console.log(m));
+
+
+ws_in.on('close', (code, reason) => {
+  console.log(code, reason.toString());
+  console.log('Connection closed');
+  // close
 });
 
 // handle diffs & persist db
-ws.on('message', (m) => {
-  let diff = JSON.parse(m);
-  console.log(`received diff: ${JSON.stringify(diff)}`);
-
-  log.appendFile(JSON.stringify(diff) + '\n');
-  db = apply(db, diff);
-  
-  console.log(`new db state: ${JSON.stringify(db)}`);
-  ws.send(JSON.stringify(db));
+ws_in.on('message', (m) => {
+  try {
+    let diff = JSON.parse(m);
+    console.log(`received diff: ${JSON.stringify(diff)}`);
+    log.appendFile(m + '\n');
+    db = apply(db, diff);
+    console.log(`new db state: ${JSON.stringify(db)}`);
+  } catch (e) {}
+  ws_out.send(JSON.stringify(db));
 });
+
